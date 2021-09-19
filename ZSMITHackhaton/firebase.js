50cfd95b039198a574eb721f8a9025b6d4bfe24c
ZahakHussain <57323773+ZahakHussain@users.noreply.github.com>
2021-09-19 20:34:03 +0500

    Add files via upload

---
+++ ZSMITHackhaton/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0mGB2c9979YYgE69W4KXkuNMpzYk44uA",
+    authDomain: "smit-hackathoon.firebaseapp.com",
+    projectId: "smit-hackathoon",
+    storageBucket: "smit-hackathoon.appspot.com",
+    messagingSenderId: "370061751987",
+    appId: "1:370061751987:web:796ff7028961d0086de2c6",
+    measurementId: "G-5F56562NCB"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ ZSMITHackhaton/welcome.js
@@ -0,0 +1,41 @@
+firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("index.html")
+    }else{
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    }
+})
+
+
+function logout(){
+    firebase.auth().signOut()
+}
+let popped = 0;
+
+document.addEventListener('mouseover', function(e){
+    
+    if (e.target.className === "balloon"){
+        
+                e.target.style.backgroundColor = "#ededed";
+                e.target.textContent = "POP!";
+                popped++;
+                removeEvent(e);
+                checkAllPopped();
+    }   
+});
+
+function removeEvent(e){
+    e.target.removeEventListener('mouseover', function(){
+        
+    })
+};
+
+function checkAllPopped(){
+    if (popped === 24){
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
